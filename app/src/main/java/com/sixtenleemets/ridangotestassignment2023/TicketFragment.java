832444a79b84f000ce033161e1d47fb54ae10090
Sixten Leemets <sixten@onoffapp.com>
2023-06-15 14:58:30 +0300

    Integrate TicketViewModel to TicketFragment

--- app/src/main/java/com/sixtenleemets/ridangotestassignment2023/TicketFragment.java
+++ app/src/main/java/com/sixtenleemets/ridangotestassignment2023/TicketFragment.java
@@ -3,6 +3,7 @@ package com.sixtenleemets.ridangotestassignment2023;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.sixtenleemets.ridangotestassignment2023.data.model.Ticket;
 import com.sixtenleemets.ridangotestassignment2023.databinding.FragmentTicketBinding;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+@AndroidEntryPoint
 public class TicketFragment extends Fragment {
+
+    private static final String TAG = "TicketFragment";
+
+    @Inject
+    TicketViewModel ticketViewModel;
 
     private FragmentTicketBinding binding;
 
@@ -31,12 +50,42 @@ public class TicketFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonSell.setOnClickListener(button ->
-                NavHostFragment.findNavController(TicketFragment.this)
-                        .navigate(R.id.action_TicketFragment_to_DashboardFragment));
         binding.productName.addTextChangedListener(getTextWatcher());
         binding.price.addTextChangedListener(getTextWatcher());
+        binding.buttonSell.setOnClickListener(button -> {
+            button.setEnabled(false);
+            String productName = binding.productName.getText().toString();
+            String productPrice = binding.price.getText().toString();
+            Ticket ticket = new Ticket(productName, Float.parseFloat(productPrice));
+            addTicket(ticket);
+        });
     }
+
+    private void addTicket(Ticket ticket) {
+        Completable.fromAction(() -> ticketViewModel.addTicket(ticket))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        NavHostFragment.findNavController(TicketFragment.this)
+                                .navigate(R.id.action_TicketFragment_to_DashboardFragment);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Snackbar.make(binding.buttonSell, e.toString(), BaseTransientBottomBar.LENGTH_LONG)
+                                .show();
+                        Log.w(TAG, "addTicket onError: ", e);
+                        binding.buttonSell.setEnabled(true);
+                    }
+                });
+    }
+
 
     @NonNull
     private TextWatcher getTextWatcher() {
